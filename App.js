586ca8fbf34c81226f427f52649d83ac3bf3b810
Surabhi Prajapati <127585333+Surabhi-7@users.noreply.github.com>
2024-05-21 08:26:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import Header from "./WebsiteLayout/Header/Header";
+import NavBar from "./WebsiteLayout/Navbar/NavBar";
+import Carousel from "./WebsiteLayout/Carousel/Carousel";
+import MainSec from "./WebsiteLayout/MainSection/MainSec";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <Carousel />
+      <MainSec />
+    </>
+  );
+}
+
+export default App;
+
+      
+      
